ad9c334f026ad87e7c2ad4e39b2c2d601c6fbc28
brunomoura <brunomourapaz@gmail.com>
2022-10-05 16:58:02 -0300

    Initial commit

---
+++ src/main/java/br/com/futurodev/primeiraapi/Application.java
@@ -0,0 +1,16 @@
+package br.com.futurodev.primeiraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // É a linha principal que roda o projeto Java Spring Boot
+        SpringApplication.run(Application.class, args);
+    }
+}
